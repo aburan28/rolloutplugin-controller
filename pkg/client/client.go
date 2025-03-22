3e491d55aed72baaf332ed7360cefd92104280df
Adam Buran <aburan28@gmail.com>
2025-03-22 13:36:04 -0700

    more

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,18 +3,20 @@ package client
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 	"sync"
 
-	"github.com/aburan28/rolloutplugin-controller/pkg/plugin"
 	"github.com/aburan28/rolloutplugin-controller/pkg/plugin/rpc"
 	types "github.com/aburan28/rolloutplugin-controller/pkg/types"
+	"github.com/argoproj/argo-rollouts/utils/config"
+	"github.com/argoproj/argo-rollouts/utils/defaults"
 
 	goPlugin "github.com/hashicorp/go-plugin"
 )
 
 type RolloutPlugin struct {
 	client map[string]*goPlugin.Client
-	plugin map[string]plugin.RolloutPlugin
+	plugin map[string]rpc.RolloutPlugin
 }
 
 var pluginClients *RolloutPlugin
@@ -24,7 +26,7 @@ var mutex sync.Mutex
 var handshakeConfig = goPlugin.HandshakeConfig{
 	ProtocolVersion:  1,
 	MagicCookieKey:   "ARGO_ROLLOUTS_RPC_PLUGIN",
-	MagicCookieValue: "step",
+	MagicCookieValue: "statefulset",
 }
 
 // pluginMap is the map of plugins we can dispense.
@@ -33,21 +35,44 @@ var pluginMap = map[string]goPlugin.Plugin{
 	"RpcRolloutPlugin": &rpc.RpcRolloutPlugin{},
 }
 
+func GetPluginInfo(pluginName string, pluginType types.PluginType) (string, []string, error) {
+	configMap, err := config.GetConfig()
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to get config: %w", err)
+	}
+
+	plugin := configMap.GetPlugin(pluginName, pluginType)
+	if plugin == nil {
+		return "", nil, fmt.Errorf("plugin %s not configured in configmap", pluginName)
+	}
+
+	dir, filename, err := config.GetPluginDirectoryAndFilename(plugin.Name)
+	if err != nil {
+		return "", nil, err
+	}
+	absFilePath, err := filepath.Abs(filepath.Join(defaults.DefaultRolloutPluginFolder, dir, filename))
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to get absolute path of plugin folder: %w", err)
+	}
+	return absFilePath, plugin.Args, nil
+
+}
+
 func (t *RolloutPlugin) startPlugin(pluginName string) (rpc.RolloutPlugin, error) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
 	if t.client[pluginName] == nil || t.client[pluginName].Exited() {
 
-		pluginPath, args, err := plugin.GetPluginInfo(pluginName, types.RolloutPluginStep)
-		if err != nil {
-			return nil, fmt.Errorf("unable to find plugin (%s): %w", pluginName, err)
-		}
-
+		// pluginPath, args, err := plugin.GetPluginInfo(pluginName, types.PluginTypeRollout)
+		// if err != nil {
+		// 	return nil, fmt.Errorf("unable to find plugin (%s): %w", pluginName, err)
+		// }
+		pluginPath := "rolloutplugin"
 		t.client[pluginName] = goPlugin.NewClient(&goPlugin.ClientConfig{
 			HandshakeConfig: handshakeConfig,
 			Plugins:         pluginMap,
-			Cmd:             exec.Command(pluginPath, args...),
+			Cmd:             exec.Command(pluginPath),
 			Managed:         true,
 		})
 
